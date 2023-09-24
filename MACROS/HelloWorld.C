2d0361b604dedf8cf1dd2bcb8759c48ec41851be
Tom <thomas.fergy@gmail.com>
2023-09-24 18:09:36 +0100

    Add test macro

---
+++ MACROS/HelloWorld.C
@@ -0,0 +1,10 @@
+#include <iostream>
+
+// #include "TBrowser.h"
+
+void HelloWorld() {
+  for (int i = 0; i < 10; i++) {
+    std::cout << "Hello World!" << std::endl;
+    std::cout << i << std::endl;
+  }
+}
