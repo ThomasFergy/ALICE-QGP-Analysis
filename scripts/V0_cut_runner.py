c8409eafc2532817af8319ef8085ca5a6e50924b
Thomas Ferguson <ThomasFerguson01@outlook.com>
2023-11-14 19:02:29 +0000

    black python format

--- scripts/V0_cut_runner.py
+++ scripts/V0_cut_runner.py
@@ -1,4 +1,4 @@
-'''
+"""
 Script to batch apply cut values to V0s for further processing.
 --- (requires being in the alienv environment before running) ---
 
@@ -7,7 +7,7 @@ Method:
   Step 2: Run the run_step0(_MC).sh script to produce the output files
   Step 3: Rename the output file to avoid overwriting and place into relevant output directory
   Step 4: go back to step 1 and repeat until you have applied all the cuts you want
-'''
+"""
 
 import os
 import sys
@@ -17,74 +17,85 @@ import subprocess
 isMC = True
 
 if isMC:
-  cwd = "data/V0MC"
-  output_dir = "output/V0MC"
-  json_file = '{}/json_strangenesstutorialMC_sig.json'.format(cwd)
+    cwd = "data/V0MC"
+    output_dir = "output/V0MC"
+    json_file = "{}/json_strangenesstutorialMC_sig.json".format(cwd)
 else:
-  cwd = "data/V0Data"
-  output_dir = "output/V0Data"
-  json_file = '{}/json_strangenesstutorial.json'.format(cwd)
+    cwd = "data/V0Data"
+    output_dir = "output/V0Data"
+    json_file = "{}/json_strangenesstutorial.json".format(cwd)
 
 
-cut_parameters = ["dcanegtopv","dcapostopv", "v0cospa", "dcav0dau", "v0radius"]
+cut_parameters = ["dcanegtopv", "dcapostopv", "v0cospa", "dcav0dau", "v0radius"]
 
 default_cut_values = [0.0, 0.0, 0.9, 1, 0.899999976]
 
+
 def set_cut_value(json_file, cut_name_index, cut_value):
-    '''
+    """
     Function to set the cut value in the json file
-    '''
-    with open(json_file, 'r') as f:
+    """
+    with open(json_file, "r") as f:
         data = json.load(f)
-    data['lambdakzero-builder'][cut_parameters[cut_name_index]] = str(cut_value)
-    with open(json_file, 'w') as f:
+    data["lambdakzero-builder"][cut_parameters[cut_name_index]] = str(cut_value)
+    with open(json_file, "w") as f:
         json.dump(data, f, indent=2)
 
 
 if __name__ == "__main__":
+    # set default cut values
+    for i in range(len(cut_parameters)):
+        set_cut_value(json_file, i, default_cut_values[i])
 
-  # set default cut values
-  for i in range(len(cut_parameters)):
-    set_cut_value(json_file, i, default_cut_values[i])
+    # Set the cut value in the json file
+    # 0 = dcanegtopv
+    # 1 = dcapostopv
+    # 2 = v0cospa
+    # 3 = dcav0dau
+    # 4 = v0radius
 
-  # Set the cut value in the json file
-  # 0 = dcanegtopv
-  # 1 = dcapostopv
-  # 2 = v0cospa
-  # 3 = dcav0dau
-  # 4 = v0radius
+    par_index = 0
+    cut_values = [0, 0.02, 0.04, 0.06, 0.08]
 
-  par_index = 0
-  cut_values = [0, 0.02, 0.04, 0.06, 0.08]
+    for cut_value in cut_values:
+        set_cut_value(json_file, par_index, cut_value)
 
-  for cut_value in cut_values:
-    set_cut_value(json_file, par_index, cut_value)
+        # check if output file has already been produced
+        if os.path.isfile(
+            "{}/AnalysisResults_{}_{}.root".format(
+                output_dir, cut_parameters[par_index], cut_value
+            )
+        ):
+            print(
+                "Output file already exists for cut {} = {}".format(
+                    cut_parameters[par_index], cut_value
+                )
+            )
+            print("Skipping...")
+            print()
+            continue
 
-    # check if output file has already been produced
-    if os.path.isfile("{}/AnalysisResults_{}_{}.root".format(output_dir, cut_parameters[par_index], cut_value)):
-      print("Output file already exists for cut {} = {}".format(cut_parameters[par_index], cut_value))
-      print("Skipping...")
-      print()
-      continue
+        print("Running cut {} = {}".format(cut_parameters[par_index], cut_value))
+        print()
 
-    print("Running cut {} = {}".format(cut_parameters[par_index], cut_value))
-    print()
+        # Apply the cut (requires being in the alienv environment before running)
+        if isMC:
+            bash_script = "./run_step0_MC.sh"
+        else:
+            bash_script = "./run_step0.sh"
 
-    # Apply the cut (requires being in the alienv environment before running)
-    if isMC:
-       bash_script = "./run_step0_MC.sh"
-    else:
-        bash_script = "./run_step0.sh"
+        # log the command to be run in a temp file in case of error
+        result = subprocess.run([bash_script], cwd=cwd)
+        if result.returncode != 0:
+            print("Error running bash script: {}".format(bash_script))
+            print("Exiting...")
+            sys.exit(1)
 
+        # Rename the output file to avoid overwriting
+        os.system(
+            "mv {}/AnalysisResults.root {}/AnalysisResults_{}_{}.root".format(
+                cwd, output_dir, cut_parameters[par_index], cut_value
+            )
+        )
 
-    # log the command to be run in a temp file in case of error
-    result = subprocess.run([bash_script], cwd=cwd)
-    if result.returncode != 0:
-      print("Error running bash script: {}".format(bash_script))
-      print("Exiting...")
-      sys.exit(1)
-
-    # Rename the output file to avoid overwriting
-    os.system("mv {}/AnalysisResults.root {}/AnalysisResults_{}_{}.root".format(cwd, output_dir, cut_parameters[par_index], cut_value))
-
-  print("Script finished successfully")
+    print("Script finished successfully")
--- scripts/significance_runner.py
+++ scripts/significance_runner.py
@@ -1,4 +1,4 @@
-'''
+"""
 Script to calculate the significance of the V0s
 --- (requires being in the alienv environment before running) ---
 --- (may require running the V0_cut_runner.py script first if no data exists yet) ---
@@ -7,7 +7,7 @@ Method:
   Step 1: Extract the cut values for the desired parameter from the output files in the output directory
   Step 2: For each parameter .root file in the output directory, run the SignificanceFit.C root script
   Step 3: save the data in a database for later use.
-'''
+"""
 
 
 import os
@@ -18,42 +18,62 @@ import subprocess
 isMC = False
 
 if isMC:
-  cwd = "data/V0MC"
-  output_dir = "output/V0MC"
+    cwd = "data/V0MC"
+    output_dir = "output/V0MC"
 else:
-  cwd = "data/V0Data"
-  output_dir = "output/V0Data"
+    cwd = "data/V0Data"
+    output_dir = "output/V0Data"
 
-cut_parameters = ["dcanegtopv","dcapostopv", "v0cospa", "dcav0dau", "v0radius"]
+cut_parameters = ["dcanegtopv", "dcapostopv", "v0cospa", "dcav0dau", "v0radius"]
 
-K0_results = {"dcanegtopv": {}, "dcapostopv": {}, "v0cospa": {}, "dcav0dau": {}, "v0radius": {}}
-lambda_results = {"dcanegtopv": {}, "dcapostopv": {}, "v0cospa": {}, "dcav0dau": {}, "v0radius": {}}
+K0_results = {
+    "dcanegtopv": {},
+    "dcapostopv": {},
+    "v0cospa": {},
+    "dcav0dau": {},
+    "v0radius": {},
+}
+lambda_results = {
+    "dcanegtopv": {},
+    "dcapostopv": {},
+    "v0cospa": {},
+    "dcav0dau": {},
+    "v0radius": {},
+}
 
 cpp_convert = {True: "true", False: "false"}
-  
+
 
 def get_cut_values(dir, cut_parameter_index):
-  files = os.listdir(dir)
-  # get all files with the cut_parameter in the name
-  files = [f for f in files if cut_parameters[cut_parameter_index] in f]
-  cut_values = []
-  # cut values are the last part of the file name after the underscore
-  for f in files:
-    cut_values.append(float(f.split("_")[-1][:-5]))
-  files_sorted = [x for _, x in sorted(zip(cut_values, files))]
-  cut_values.sort()
-  return [cut_values, files_sorted]
+    files = os.listdir(dir)
+    # get all files with the cut_parameter in the name
+    files = [f for f in files if cut_parameters[cut_parameter_index] in f]
+    cut_values = []
+    # cut values are the last part of the file name after the underscore
+    for f in files:
+        cut_values.append(float(f.split("_")[-1][:-5]))
+    files_sorted = [x for _, x in sorted(zip(cut_values, files))]
+    cut_values.sort()
+    return [cut_values, files_sorted]
+
 
 if __name__ == "__main__":
-  # 0 = dcanegtopv
-  # 1 = dcapostopv
-  # 2 = v0cospa
-  # 3 = dcav0dau
-  # 4 = v0radius
-  cuts, files = get_cut_values(output_dir, 0)
+    # 0 = dcanegtopv
+    # 1 = dcapostopv
+    # 2 = v0cospa
+    # 3 = dcav0dau
+    # 4 = v0radius
+    cuts, files = get_cut_values(output_dir, 0)
 
-
-  # not generalised at all yet
-  fit_params = "{1.2, 1.2, 60, 30, 0.49, 0.004, 3000, 1, 1, 1, 1}"
-  args = "{}, {}, {}, {}, {}".format(cpp_convert[isMC], str(0.46), str(0.54), '"'+str("{}/".format(output_dir)+files[0])+'"', fit_params)
-  result = subprocess.run(['root', '-l', '-b', '-q', "MACROS/SignificanceFit.C({})".format(args)]) # will want to pipe this probably
+    # not generalised at all yet
+    fit_params = "{1.2, 1.2, 60, 30, 0.49, 0.004, 3000, 1, 1, 1, 1}"
+    args = "{}, {}, {}, {}, {}".format(
+        cpp_convert[isMC],
+        str(0.46),
+        str(0.54),
+        '"' + str("{}/".format(output_dir) + files[0]) + '"',
+        fit_params,
+    )
+    result = subprocess.run(
+        ["root", "-l", "-b", "-q", "MACROS/SignificanceFit.C({})".format(args)]
+    )  # will want to pipe this probably
