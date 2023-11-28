c2b1f6a0aceff56a8a13a55e323457cf70e41458
Thomas Ferguson <ThomasFerguson01@outlook.com>
2023-11-28 12:03:45 +0000

    Add all K0 Fitted values change cut range for parameter plots

--- scripts/significance_plot.py
+++ scripts/significance_plot.py
@@ -7,6 +7,17 @@ cut_parameters = ["dcanegtopv", "dcapostopv", "v0cospa", "dcav0dau", "v0radius"]
 
 V0_names = ["K0Data", "LambdaData", "K0MC", "LambdaMC"]
 
+# min and max values for each cut parameter
+cut_ranges = {
+    "K0Data": {
+        "dcanegtopv": [0, 0.25],
+        "dcapostopv": [0, 0.25],
+        "v0cospa": [0.985, 1],
+        "dcav0dau": [0, 1],
+        "v0radius": [0, 0.875],
+    },
+}
+
 
 def make_plot(V0_index, cut_index):
     if V0_index > 1:
@@ -60,15 +71,77 @@ def make_plot(V0_index, cut_index):
             ]
         )
 
+    # filter using the cut ranges
+    data_significance_values = np.array(data_significance_values)
+    data_significance_errors = np.array(data_significance_errors)
+    data_signal_values = np.array(data_signal_values)
+    data_signal_errors = np.array(data_signal_errors)
+    MC_significance_values = np.array(MC_significance_values)
+    MC_significance_errors = np.array(MC_significance_errors)
+    MC_signal_values = np.array(MC_signal_values)
+    MC_signal_errors = np.array(MC_signal_errors)
+    cuts = np.array(cuts)
+    print(cuts)
+    # filter using the cut ranges
+    data_significance_values = data_significance_values[
+        (cuts > cut_ranges[V0_names[V0_index]][cut_parameters[cut_index]][0])
+        & (cuts < cut_ranges[V0_names[V0_index]][cut_parameters[cut_index]][1])
+    ]
+    data_significance_errors = data_significance_errors[
+        (cuts > cut_ranges[V0_names[V0_index]][cut_parameters[cut_index]][0])
+        & (cuts < cut_ranges[V0_names[V0_index]][cut_parameters[cut_index]][1])
+    ]
+    data_signal_values = data_signal_values[
+        (cuts > cut_ranges[V0_names[V0_index]][cut_parameters[cut_index]][0])
+        & (cuts < cut_ranges[V0_names[V0_index]][cut_parameters[cut_index]][1])
+    ]
+    data_signal_errors = data_signal_errors[
+        (cuts > cut_ranges[V0_names[V0_index]][cut_parameters[cut_index]][0])
+        & (cuts < cut_ranges[V0_names[V0_index]][cut_parameters[cut_index]][1])
+    ]
+    MC_significance_values = MC_significance_values[
+        (cuts > cut_ranges[V0_names[V0_index]][cut_parameters[cut_index]][0])
+        & (cuts < cut_ranges[V0_names[V0_index]][cut_parameters[cut_index]][1])
+    ]
+    MC_significance_errors = MC_significance_errors[
+        (cuts > cut_ranges[V0_names[V0_index]][cut_parameters[cut_index]][0])
+        & (cuts < cut_ranges[V0_names[V0_index]][cut_parameters[cut_index]][1])
+    ]
+    MC_signal_values = MC_signal_values[
+        (cuts > cut_ranges[V0_names[V0_index]][cut_parameters[cut_index]][0])
+        & (cuts < cut_ranges[V0_names[V0_index]][cut_parameters[cut_index]][1])
+    ]
+    MC_signal_errors = MC_signal_errors[
+        (cuts > cut_ranges[V0_names[V0_index]][cut_parameters[cut_index]][0])
+        & (cuts < cut_ranges[V0_names[V0_index]][cut_parameters[cut_index]][1])
+    ]
+    cuts = cuts[
+        (cuts > cut_ranges[V0_names[V0_index]][cut_parameters[cut_index]][0])
+        & (cuts < cut_ranges[V0_names[V0_index]][cut_parameters[cut_index]][1])
+    ]
+    print(cuts)
+
+    # flip the arrays if cut_index is 3
+    if cut_index == 3:
+        data_significance_values = np.flip(data_significance_values)
+        data_significance_errors = np.flip(data_significance_errors)
+        data_signal_values = np.flip(data_signal_values)
+        data_signal_errors = np.flip(data_signal_errors)
+        MC_significance_values = np.flip(MC_significance_values)
+        MC_significance_errors = np.flip(MC_significance_errors)
+        MC_signal_values = np.flip(MC_signal_values)
+        MC_signal_errors = np.flip(MC_signal_errors)
+        cuts = np.flip(cuts)
+
     # convert significance values to string
-    data_significance_values = str(data_significance_values)
-    data_significance_errors = str(data_significance_errors)
-    data_signal_values = str(data_signal_values)
-    data_signal_errors = str(data_signal_errors)
-    MC_significance_values = str(MC_significance_values)
-    MC_significance_errors = str(MC_significance_errors)
-    MC_signal_values = str(MC_signal_values)
-    MC_signal_errors = str(MC_signal_errors)
+    data_significance_values = str(list(data_significance_values))
+    data_significance_errors = str(list(data_significance_errors))
+    data_signal_values = str(list(data_signal_values))
+    data_signal_errors = str(list(data_signal_errors))
+    MC_significance_values = str(list(MC_significance_values))
+    MC_significance_errors = str(list(MC_significance_errors))
+    MC_signal_values = str(list(MC_signal_values))
+    MC_signal_errors = str(list(MC_signal_errors))
     # make square brackets curly
     data_significance_values = data_significance_values.replace("[", "{")
     data_significance_values = data_significance_values.replace("]", "}")
