2456e4965349049e243040f1307f76852653698c
Thomas Ferguson <ThomasFerguson01@outlook.com>
2023-12-02 12:35:21 +0000

    Add meta data macro

---
+++ PrintMetaData.C
@@ -0,0 +1,11 @@
+#include <TFile.h>
+#include <TTree.h>
+
+void PrintMetaData() {
+  /**
+   * Simple macro to print the AO2D.root metaData
+   */
+  TFile *f = new TFile("data/AO2D.root");
+  TDirectory *dir = (TDirectory *)f->Get("metaData");
+  dir->Print();
+}
