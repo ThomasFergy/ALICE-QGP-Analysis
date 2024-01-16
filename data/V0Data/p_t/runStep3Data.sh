3ddf8ef6319e4b72944c26ee098796f88f86454b
Thomas Ferguson <ThomasFerguson01@outlook.com>
2024-01-16 11:29:12 +0000

    Add new files for p_t

---
+++ data/V0Data/p_t/runStep3Data.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# log file where the terminal output will be saved
+STEP="step3Data"
+LOGFILE="log-${STEP}.txt"
+
+# directory of this script
+DIR_THIS="$(dirname "$(realpath "$0")")"
+
+OPTION="-b --configuration json:/$DIR_THIS/jsonConfigs/$STEP.json --resources-monitoring 2 --aod-memory-rate-limit 1000000000 --shm-segment-size 6000000000"
+
+o2-analysis-timestamp ${OPTION} \
+| o2-analysis-event-selection ${OPTION} \
+| o2-analysis-bc-converter ${OPTION} \
+| o2-analysis-tracks-extra-converter  ${OPTION} \
+| o2-analysis-track-propagation ${OPTION} \
+| o2-analysis-pid-tpc ${OPTION} \
+| o2-analysis-pid-tpc-base ${OPTION} \
+| o2-analysis-pid-tpc-full ${OPTION} \
+| o2-analysis-lf-lambdakzerobuilder ${OPTION} \
+| o2-analysistutorial-lf-strangeness-step3 ${OPTION} \
+> "$LOGFILE" 2>&1
+
+# report status
+rc=$?
+if [ $rc -eq 0 ]; then
+  echo "No problems!"
+  mkdir -p "$DIR_THIS/results/$STEP"
+  mv "AnalysisResults.root" "$DIR_THIS/results/$STEP/AnalysisResults.root"
+  mv "dpl-config.json" "$DIR_THIS/jsonConfigs/$STEP.json"
+else
+  echo "Error: Exit code $rc"
+  echo "Check the log file $LOGFILE"
+  exit $rc
+fi
