e2799fcd06bb1cb6c53da660612548052902f431
Thomas Ferguson <ThomasFerguson01@outlook.com>
2024-01-23 11:50:46 +0000

    Update metadata MACRO

--- PrintMetaData.C
+++ PrintMetaData.C
@@ -5,7 +5,7 @@ void PrintMetaData() {
   /**
    * Simple macro to print the AO2D.root metaData
    */
-  TFile *f = new TFile("data/AO2D.root");
+  TFile *f = new TFile("data/V0Data/AO2D.root");
   TDirectory *dir = (TDirectory *)f->Get("metaData");
   dir->Print();
 }
