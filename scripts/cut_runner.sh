77d4e701515ac9974754c2c82e12d9cb218ab5ea
Thomas Ferguson <ThomasFerguson01@outlook.com>
2024-01-27 19:42:51 +0000

    Update bash script

--- scripts/cut_runner.sh
+++ scripts/cut_runner.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/bash
 
+# Show start time
+echo Start time : $(date)
+
 # Run the second Python script in the second alienv environment
 alienv enter O2Physics/latest-master-o2 << EOF
 python3 scripts/V0MC_cut_runner.py
@@ -11,3 +14,6 @@ alienv enter O2Physics/latest-rl-o2 << EOF
 python3 scripts/V0Data_cut_runner.py
 exit
 EOF
+
+# Show end time
+echo End time : $(date)
