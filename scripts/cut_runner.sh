1d126a5a0d060232f51eff770ab142c7630bd3c5
Thomas Ferguson <ThomasFerguson01@outlook.com>
2024-02-04 14:42:23 +0000

    Save

--- scripts/cut_runner.sh
+++ scripts/cut_runner.sh
@@ -3,17 +3,9 @@
 # Show start time
 echo Start time : $(date)
 
-# Run the second Python script in the second alienv environment
-alienv enter O2Physics/latest-rl-o2 << EOF
-python3 scripts/V0MC_cut_runner.py
-exit
-EOF
-
-# Run the first Python script in the first alienv environment
-alienv enter O2Physics/latest-master-o2 << EOF
 python3 scripts/V0Data_cut_runner.py
-exit
-EOF
+
+python3 scripts/V0MC_cut_runner.py
 
 # Show end time
 echo End time : $(date)
